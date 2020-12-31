6083e9174525c435149800dee9a920766e10cabb
Veena305 <73694304+Veena305@users.noreply.github.com>
2020-12-31 15:42:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+var dog,dogHappy;
+
+var database;
+
+
+var foodS,foodStock;
+
+function preload()
+{
+
+  dogImage = loadImage("dogImg.png");
+
+  dogHappyImage = loadImage("dogImg1.png");
+	
+}
+
+function setup() {
+
+  database = firebase.database();
+
+  createCanvas(500,500);
+  
+  dog = createSprite(250,250,150,150);
+  dog.addImage(dogImage);
+  dog.scale = 0.15;
+
+  foodStock = database.ref('Food');
+  foodStock.on("value",readStock);
+
+
+  
+}
+
+
+function draw() {  
+
+  background(46,139,87);
+
+  if(keyWentDown(UP_ARROW)) {
+
+    writeStock(foodS);
+    dog.addImage(dogHappyImage);
+
+  }
+
+  drawSprites();
+
+  stroke("black");
+  text("Food Remaining : "+ foodS,170,200);
+  textSize(12);
+
+  text("Note : Press UP_ARROW Key to feed the Dog",130,10,300,20);
+
+}
+
+function readStock(data) {
+
+  foodS = data.val();
+
+}
+
+function writeStock(x) {
+
+  if(x <= 0){
+
+    x = 0;
+
+  } 
+
+  else{
+
+    x = x - 1;
+
+  }
+  
+
+  database.ref('/').update({
+
+    Food : x
+
+  });
+
+}
+
+
+
